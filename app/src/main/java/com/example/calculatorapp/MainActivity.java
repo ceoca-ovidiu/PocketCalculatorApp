b8444f1e65d84662a15ff3f5ea01f8453b563a3e
ceoca-ovidiu <ceoca.ovidiu@gmail.com>
2020-11-02 11:27:54 +0200

    Defining some buttons

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Button zeroButton = findViewById(R.id.zeroButton);
+        Button oneButton = findViewById(R.id.oneButton);
+        Button twoButton = findViewById(R.id.twoButton);
+
         setContentView(R.layout.activity_main);
+
     }
+    
 }
