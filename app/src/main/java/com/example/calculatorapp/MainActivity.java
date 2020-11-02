a43488e9683605242e2ed2f79bc7c8f05d5554c4
ceoca-ovidiu <ceoca.ovidiu@gmail.com>
2020-11-02 14:51:23 +0200

    Defining some buttons in a separate function

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -10,13 +10,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeButtons();
+        setContentView(R.layout.activity_main);
+
+    }
+
+    void initializeButtons(){
 
         Button zeroButton = findViewById(R.id.zeroButton);
         Button oneButton = findViewById(R.id.oneButton);
         Button twoButton = findViewById(R.id.twoButton);
-
-        setContentView(R.layout.activity_main);
+        Button threeButton = findViewById(R.id.threeButton);
+        Button fourButton = findViewById(R.id.fourButton);
+        Button fiveButton = findViewById(R.id.fiveButton);
+        Button sixButton = findViewById(R.id.sixButton);
+        Button sevenButton = findViewById(R.id.sevenButton);
+        Button eightButton = findViewById(R.id.eightButton);
+        Button nineButton = findViewById(R.id.nineButton);
+        Button plusButton = findViewById(R.id.plusButton);
+        Button minusButton = findViewById(R.id.minusButton);
+        Button multiplyButton = findViewById(R.id.multiplyButton);
+        Button divideButton = findViewById(R.id.divideButton);
+        Button moduloButton = findViewById(R.id.moduloButton);
+        Button radicalButton = findViewById(R.id.sqrtButton);
+        Button piButton = findViewById(R.id.piButton);
+        Button dotButton = findViewById(R.id.dotButton);
+        Button equalButton = findViewById(R.id.equalButton);
+        Button clearButton = findViewById(R.id.clearButton);
 
     }
-    
+
 }
