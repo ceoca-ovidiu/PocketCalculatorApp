9f65943e33bd8d27bbd56419a9732007d33e26e7
ceoca-ovidiu <ceoca.ovidiu@gmail.com>
2020-11-03 15:47:34 +0200

    Made the buttons work and display something

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -2,48 +2,26 @@ package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView displaytextView;
-    Button zeroButton;
-    Button oneButton ;
-    Button twoButton;
-    Button threeButton ;
-    Button fourButton ;
-    Button fiveButton;
-    Button sixButton ;
-    Button sevenButton ;
-    Button eightButton ;
-    Button nineButton ;
-    Button plusButton;
-    Button minusButton ;
-    Button multiplyButton ;
-    Button divideButton ;
-    Button moduloButton ;
-    Button radicalButton ;
-    Button piButton ;
-    Button dotButton ;
-    Button equalButton ;
-    Button clearButton ;
 
-
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initializeButtons();
         setContentView(R.layout.activity_main);
-       // buttonsListener(View view);
-
-    }
-
-    void initializeButtons(){
-
-        TextView displaytextView = findViewById(R.id.firstNumberTextView);
+        final TextView operationtextView = findViewById(R.id.operationTextView);
+        final TextView firstNumberPlainText = findViewById(R.id.firstNumberPlainText);
+        final TextView secondNumberPlainText = findViewById(R.id.secondNumberPlainText);
+        TextView resulttextView = findViewById(R.id.resultTextView);
         Button zeroButton = findViewById(R.id.zeroButton);
         Button oneButton = findViewById(R.id.oneButton);
         Button twoButton = findViewById(R.id.twoButton);
@@ -64,11 +42,155 @@ public class MainActivity extends AppCompatActivity {
         Button dotButton = findViewById(R.id.dotButton);
         Button equalButton = findViewById(R.id.equalButton);
         Button clearButton = findViewById(R.id.clearButton);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operationtextView.setText("*");
+            }
+        });
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operationtextView.setText("-");
+            }
+        });
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operationtextView.setText("/");
+            }
+        });
+        moduloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operationtextView.setText("%");
+            }
+        });
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operationtextView.setText("+");
+            }
+        });
+        firstNumberPlainText.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                firstNumberPlainText.setInputType(InputType.TYPE_NULL);
+                return false;
+            }
+        });
+        secondNumberPlainText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                secondNumberPlainText.setInputType(InputType.TYPE_NULL);
+                return false;
+            }
+        });
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("1");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("1");
+                }
 
+            }
+        });
+        twoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("2");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("2");
+                }
+            }
+        });
+        threeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("3");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("3");
+                }
+            }
+        });
+        fourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("4");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("4");
+                }
+            }
+        });
+        fiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("5");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("5");
+                }
+            }
+        });
+        sixButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("6");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("6");
+                }
+            }
+        });
+
+        sevenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("7");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("7");
+                }
+            }
+        });
+
+        eightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("8");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("8");
+                }
+            }
+        });
+
+        nineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstNumberPlainText.hasFocus()){
+                    firstNumberPlainText.setText("9");
+                }
+                if(secondNumberPlainText.hasFocus()){
+                    secondNumberPlainText.setText("9");
+                }
+            }
+        });
     }
 
-   public void buttonsListener(View view){
-
-   }
 
 }
